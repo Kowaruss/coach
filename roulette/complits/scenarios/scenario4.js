ea8bb0a1be3b3425b2a157cd4b14df0ba33155b8
Kowaruss <76119984+Kowaruss@users.noreply.github.com>
2025-10-01 14:19:28 +0300

    Create scenario4.js

---
+++ roulette/complits/scenarios/scenario4.js
@@ -0,0 +1,6 @@
+function generateScenario4(bet) {
+    return {
+        text: `Сценарий 4<br>по ${bet}`,
+        answer: `<span class="label">Ставка:</span><br>Сценарий 4 ставка<br><span class="label">Выплата:</span> сценарий 4 выплата по ${bet}`
+    };
+}
