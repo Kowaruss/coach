18105669a92fd5d8dabd388e2369b1f83fba9bdd
Kowaruss <76119984+Kowaruss@users.noreply.github.com>
2025-10-01 12:34:47 +0300

    создал сценарий для дюжин

---
+++ roulette/complits/scenarios/scenario1.js
@@ -0,0 +1,9 @@
+function generateScenario1(bet) {
+    const dozensMultipliers = {1: 136, 2: 140, 3: 120};
+    const complit = Math.floor(Math.random() * 3) + 1;
+    
+    return {
+        text: `Комплит ${complit} дюжины<br>по ${bet}`,
+        answer: `<span class="label">Ставка:</span> ${formatNumber(dozensMultipliers[complit] * bet)}<br><span class="label">Выплата:</span> комплит выпавшего номера по ${bet}`
+    };
+}
