c3c6dfc2a620eaa7d7db94d5ae990180f876eedb
Kowaruss <76119984+Kowaruss@users.noreply.github.com>
2025-10-01 14:25:18 +0300

    Create scenario7.js

---
+++ roulette/complits/scenarios/scenario7.js
@@ -0,0 +1,6 @@
+function generateScenario7(bet) {
+    return {
+        text: `Сценарий 7<br>по ${bet}`,
+        answer: `<span class="label">Ставка:</span><br>Сценарий 7 ставка<br><span class="label">Выплата:</span> сценарий 7 выплата по ${bet}`
+    };
+}
