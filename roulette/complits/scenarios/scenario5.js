945c0b9d00bdf8461da5969f0ce140d00a25fd47
Kowaruss <76119984+Kowaruss@users.noreply.github.com>
2025-10-02 04:47:15 +0300

    Delete roulette/complits/scenarios/scenario5.js

--- roulette/complits/scenarios/scenario5.js
+++
@@ -1,6 +0,0 @@
-function generateScenario5(bet) {
-    return {
-        text: `Сценарий 5<br>по ${bet}`,
-        answer: `<span class="label">Ставка:</span><br>Сценарий 5 ставка<br><span class="label">Выплата:</span> сценарий 5 выплата по ${bet}`
-    };
-}
