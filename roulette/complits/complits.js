716341a1f6b887d173ff6fb2286defc432fa6692
Kowaruss <76119984+Kowaruss@users.noreply.github.com>
2025-10-02 04:48:44 +0300

    Update complits.js

--- roulette/complits/complits.js
+++ roulette/complits/complits.js
@@ -25,39 +25,30 @@ class ComplitsGame {
     }
     
     generateExample() {
-        const randomNumber = Math.floor(Math.random() * 7) + 1;
-        const bet = this.bets[Math.floor(Math.random() * this.bets.length)];
-        
-        let scenario;
-        switch(randomNumber) {
-            case 1:
-                scenario = generateScenario1(bet);
-                break;
-            case 2:
-                scenario = generateScenario2(bet);
-                break;
-            case 3:
-                scenario = generateScenario3(bet);
-                break;
-            case 4:
-                scenario = generateScenario4(bet);
-                break;
-            case 5:
-                scenario = generateScenario5(bet);
-                break;
-            case 6:
-                scenario = generateScenario6(bet);
-                break;
-            case 7:
-                scenario = generateScenario7(bet);
-                break;
-        }
-        
-        this.complitsText.innerHTML = scenario.text;
-        this.answerElement.innerHTML = scenario.answer;
-        this.answerElement.classList.remove('show');
-        this.actionButton.reset();
+    const randomNumber = Math.floor(Math.random() * 4) + 1; // Только 1-4
+    const bet = this.bets[Math.floor(Math.random() * this.bets.length)];
+    
+    let scenario;
+    switch(randomNumber) {
+        case 1:
+            scenario = generateScenario1(bet);
+            break;
+        case 2:
+            scenario = generateScenario2(bet);
+            break;
+        case 3:
+            scenario = generateScenario3(bet);
+            break;
+        case 4:
+            scenario = generateScenario4(bet);
+            break;
     }
+    
+    this.complitsText.innerHTML = scenario.text;
+    this.answerElement.innerHTML = scenario.answer;
+    this.answerElement.classList.remove('show');
+    this.actionButton.reset();
+}
     
     showAnswer() {
         this.answerElement.classList.add('show');
