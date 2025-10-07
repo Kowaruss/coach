2178819e3644092cc5da6984c659fabda78fb9a9
Kowaruss <76119984+Kowaruss@users.noreply.github.com>
2025-10-07 10:14:56 +0300

    Update scenario2.js

--- roulette/complits/scenarios/scenario2.js
+++ roulette/complits/scenarios/scenario2.js
@@ -1,19 +1,32 @@
 function generateScenario2(bet) {
-    const sixLineMultipliers = {'first': 76, 'last': 60, 'other': 80};
-    const types = ['first', 'last', 'other', 'other', 'other', 'other'];
-    const sixLineType = types[Math.floor(Math.random() * types.length)];
+    const sixLineMultipliers = {
+        'first': 76,    // для 1-6
+        'last': 60,     // для 31-36  
+        'other': 80     // для 7-12
+    };
     
+    // Варианты six line
+    const sixLineTypes = [
+        { name: "Комплит Six Line 1 - 6", type: "first" },
+        { name: "Комплит Six Line 7 - 12", type: "other" },
+        { name: "Комплит Six Line 31 - 36", type: "last" }
+    ];
+    
+    // Выбираем случайный six line
+    const selectedSixLine = sixLineTypes[Math.floor(Math.random() * sixLineTypes.length)];
+    
+    // Определяем ставку в зависимости от выбранного six line
     let stake = 0;
-    if (sixLineType === 'first') stake = sixLineMultipliers.first * bet;
-    else if (sixLineType === 'last') stake = sixLineMultipliers.last * bet;
-    else stake = sixLineMultipliers.other * bet;
-    
-    const firstStake = formatNumber(sixLineMultipliers.first * bet);
-    const lastStake = formatNumber(sixLineMultipliers.last * bet);
-    const otherStake = formatNumber(sixLineMultipliers.other * bet);
+    if (selectedSixLine.type === "first") {
+        stake = sixLineMultipliers.first * bet;
+    } else if (selectedSixLine.type === "last") {
+        stake = sixLineMultipliers.last * bet;
+    } else {
+        stake = sixLineMultipliers.other * bet;
+    }
     
     return {
-        text: `Комплит six line<br>по ${bet}`,
-        answer: `<span class="label">Ставка:</span><br>первый six line ${firstStake}<br>последний six line ${lastStake}<br>остальные ${otherStake}<br><span class="label">Выплата:</span> комплит выпавшего номера по ${bet}`
+        text: `${selectedSixLine.name}<br>по ${bet}`,
+        answer: `<span class="label">Ставка:</span><br>${formatNumber(stake)}<br><span class="label">Выплата:</span><br>Комплит выпавшего номера по ${bet}`
     };
 }
