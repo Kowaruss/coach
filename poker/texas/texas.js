8a7b34a163f8cd3b8be7ccc27f9b115a106693fd
Kowaruss <76119984+Kowaruss@users.noreply.github.com>
2025-10-14 22:05:22 +0300

    Update texas.js

--- poker/texas/texas.js
+++ poker/texas/texas.js
@@ -16,7 +16,7 @@ function calculateTexasAnswer(combination, ante) {
     const payout = ante * combination.multiplier;
     
     return {
-        text: `Комбинация "${combination.name}"<br>Анте ${ante}`,
+        text: `Техасский покер<br>Комбинация "${combination.name}"<br>Анте ${ante}`,
         answer: `<span class="label">Ответ:</span><br>${PokerCommon.formatNumber(payout)}`
     };
 }
