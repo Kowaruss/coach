af844f2a9357db0c8280a075c7fb8416734d0f2c
Kowaruss <76119984+Kowaruss@users.noreply.github.com>
2025-10-18 16:02:27 +0300

    Update bets.js

--- roulette/bets/bets.js
+++ roulette/bets/bets.js
@@ -6,13 +6,22 @@ class BetsGame {
         this.chip1 = document.getElementById('chip1');
         this.chip2 = document.getElementById('chip2');
         this.chip3 = document.getElementById('chip3');
+        this.chip4 = document.getElementById('chip4');
+        this.chip5 = document.getElementById('chip5');
+        this.chip6 = document.getElementById('chip6');
         this.currentNumber = 1;
         
         // Сохраняем позиции фишек
         this.chipPositions = {
-            str2: { top: '50%', left: '50%' },   // Для chip1
-            sp0_2: { top: '50%', left: '35%' },  // Для chip2
-            sp0_3: { top: '50%', left: '65%' }   // Для chip3 (15% вправо от центра)
+            // Верхний ряд
+            str2: { top: '50%', left: '50%' },    // chip1
+            sp0_2: { top: '50%', left: '35%' },   // chip2
+            sp2_5: { top: '50%', left: '65%' },   // chip3
+            
+            // Нижний ряд (сдвиг на 15% вниз)
+            str2_down: { top: '65%', left: '50%' },   // chip4
+            sp0_2_down: { top: '65%', left: '35%' },  // chip5
+            sp2_5_down: { top: '65%', left: '65%' }   // chip6
         };
         
         this.actionButton = new ActionButton(
@@ -29,7 +38,10 @@ class BetsGame {
         // Позиционируем все фишки
         this.positionChip('chip1', 'str2');
         this.positionChip('chip2', 'sp0_2');
-        this.positionChip('chip3', 'sp0_3');
+        this.positionChip('chip3', 'sp2_5');
+        this.positionChip('chip4', 'str2_down');
+        this.positionChip('chip5', 'sp0_2_down');
+        this.positionChip('chip6', 'sp2_5_down');
     }
     
     // Универсальный метод для позиционирования фишки
