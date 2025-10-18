d45de247b59102e0daaf3c5cc8adbb9dfe76d500
Kowaruss <76119984+Kowaruss@users.noreply.github.com>
2025-10-18 11:52:32 +0300

    Update bets.js

--- roulette/bets/bets.js
+++ roulette/bets/bets.js
@@ -7,32 +7,24 @@ class BetsGame {
         this.chip2 = document.getElementById('chip2');
         this.currentNumber = 1;
         
-        // Позиции фишек
-        this.chipPositions = {
-            str2: { top: '50%', left: '50%' },   // Центр - для chip1
-            str3: { top: '50%', left: '30%' }    // Левее - для chip2
-        };
-        
         this.actionButton = new ActionButton(
             this.actionBtn,
             () => this.showAnswer(),
             () => this.nextExample()
         );
         
-        // УБИРАЕМ вызов positionChip - позиции уже заданы в HTML!
-        // this.positionChip('chip1', 'str2');
-        // this.positionChip('chip2', 'str3');
+        // Позиционируем фишки после загрузки
+        this.positionChips();
     }
     
-    // Оставляем метод для будущего использования
-    positionChip(chipId, positionName) {
-        const chip = document.getElementById(chipId);
-        if (chip && this.chipPositions[positionName]) {
-            const pos = this.chipPositions[positionName];
-            chip.style.top = pos.top;
-            chip.style.left = pos.left;
-            // УБИРАЕМ transform!
-        }
+    positionChips() {
+        // Центр для первой фишки
+        this.chip1.style.top = '50%';
+        this.chip1.style.left = '50%';
+        
+        // Левее для второй фишки
+        this.chip2.style.top = '50%';
+        this.chip2.style.left = '30%';
     }
     
     showAnswer() {
