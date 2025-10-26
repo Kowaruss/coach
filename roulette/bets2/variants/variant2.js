45632eb7a48502da607b12145f1958bc7897e5bf
Kowaruss <76119984+Kowaruss@users.noreply.github.com>
2025-10-26 20:10:59 +0300

    Update variant2.js

--- roulette/bets2/variants/variant2.js
+++ roulette/bets2/variants/variant2.js
@@ -1,1 +1,15 @@
-
+class Variant2 {
+    constructor() {
+        this.number = 2;
+        this.scenarios = [
+            new VoisinScenario(),
+            new TierScenario(),
+            new OrphalinsScenario(), 
+            new SpielScenario()
+        ];
+    }
+    
+    getRandomScenario() {
+        return this.scenarios[Math.floor(Math.random() * this.scenarios.length)];
+    }
+}
