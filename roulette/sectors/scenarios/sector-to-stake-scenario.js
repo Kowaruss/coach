195e5add9e08a8680520babf4f27edce5f63bd06
Kowaruss <76119984+Kowaruss@users.noreply.github.com>
2025-10-26 22:57:20 +0300

    Update sector-to-stake-scenario.js

--- roulette/sectors/scenarios/sector-to-stake-scenario.js
+++ roulette/sectors/scenarios/sector-to-stake-scenario.js
@@ -10,14 +10,27 @@ class SectorToStakeScenario {
     }
     
     generate() {
+        // Выбираем случайный тип рулетки
         const rouletteType = this.rouletteTypes[Math.floor(Math.random() * this.rouletteTypes.length)];
+        
+        // Выбираем случайный сектор
         const sectorType = this.sectorTypes[Math.floor(Math.random() * this.sectorTypes.length)];
+        
+        // Генерируем X согласно логике
         const x = this.generateX(rouletteType);
+        
+        // Формируем текст
+        const question = `${rouletteType.name}<br>"${sectorType}" по ${x}`;
+        
+        // Рассчитываем ставку
         const stake = this.calculateStake(rouletteType.name, sectorType, x);
         
+        // Формируем ответ
+        const answer = `<span class="label">Ставка:</span> ${this.formatNumber(stake)}`;
+        
         return {
-            question: `${rouletteType.name}<br>"${sectorType}" по ${x}`,
-            answer: `<span class="label">Ставка:</span> ${this.formatNumber(stake)}`
+            question: question,
+            answer: answer
         };
     }
     
@@ -28,10 +41,69 @@ class SectorToStakeScenario {
     }
     
     calculateStake(rouletteName, sectorType, x) {
-        // ... существующая логика расчета ставки
+        let stake = 0;
+        
+        if (rouletteName === "Рулетка 1 – 100") {
+            stake = this.calculateStakeFor100(sectorType, x);
+        } else if (rouletteName === "Рулетка 5 – 200") {
+            stake = this.calculateStakeFor200(sectorType, x);
+        } else {
+            stake = this.calculateStakeFor500(sectorType, x);
+        }
+        
+        return stake;
+    }
+    
+    calculateStakeFor100(sectorType, x) {
+        switch(sectorType) {
+            case "Вуазен":
+                return x <= 150 ? x * 9 : 1350 + ((x - 150) * 7);
+            case "Шпиль":
+                return x <= 100 ? x * 4 : 400 + ((x - 100) * 3);
+            case "Орфалайнс":
+                return x <= 100 ? x * 5 : 500 + ((x - 100) * 4);
+            case "Тьер":
+                return x * 6;
+            default:
+                return 0;
+        }
+    }
+    
+    calculateStakeFor200(sectorType, x) {
+        switch(sectorType) {
+            case "Вуазен":
+                return x <= 300 ? x * 9 : 2700 + ((x - 300) * 7);
+            case "Шпиль":
+                return x <= 200 ? x * 4 : 800 + ((x - 200) * 3);
+            case "Орфалайнс":
+                return x <= 200 ? x * 5 : 1000 + ((x - 200) * 4);
+            case "Тьер":
+                return x * 6;
+            default:
+                return 0;
+        }
+    }
+    
+    calculateStakeFor500(sectorType, x) {
+        switch(sectorType) {
+            case "Вуазен":
+                return x <= 750 ? x * 9 : 6750 + ((x - 750) * 7);
+            case "Шпиль":
+                return x <= 500 ? x * 4 : 2000 + ((x - 500) * 3);
+            case "Орфалайнс":
+                return x <= 500 ? x * 5 : 2500 + ((x - 500) * 4);
+            case "Тьер":
+                return x * 6;
+            default:
+                return 0;
+        }
     }
     
     formatNumber(number) {
+        if (number === undefined || number === null) {
+            console.error('formatNumber received undefined:', number);
+            return '0';
+        }
         return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
     }
 }
