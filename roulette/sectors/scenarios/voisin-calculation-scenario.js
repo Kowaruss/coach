aa07818d985f16e0f5f43c6622713ed97e50da45
Kowaruss <76119984+Kowaruss@users.noreply.github.com>
2025-10-27 23:19:42 +0300

    Update voisin-calculation-scenario.js

--- roulette/sectors/scenarios/voisin-calculation-scenario.js
+++ roulette/sectors/scenarios/voisin-calculation-scenario.js
@@ -24,12 +24,12 @@ class VoisinCalculationScenario {
         const bet = this.generateBet(betRange);
         const { playAmount, change, hasSpreading } = this.calculateVoisin(bet, baseAmount, rouletteType);
         
-        let answer = `<span class="label">Играет по:<br></span> ${this.formatNumber(playAmount)}`;
+        let answer = `<span class="label">Играет по:</br></span> ${this.formatNumber(playAmount)}`;
         if (hasSpreading) answer += " с размазыванием";
-        answer += `<br><span class="label">Сдача:<br></span> ${this.formatNumber(change)}`;
+        answer += `</br><span class="label">Сдача:</br></span> ${this.formatNumber(change)}`;
         
         return {
-            question: `${rouletteName}.<br> Ставка на Вуазен ${this.formatNumber(bet)} у.е.<br>Почем играет и сколько сдача?`,
+            question: `${rouletteName}.</br> Ставка на Вуазен ${this.formatNumber(bet)} у.е.</br>Почем играет и сколько сдача?`,
             answer: answer
         };
     }
